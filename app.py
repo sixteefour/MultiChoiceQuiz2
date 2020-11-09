9343af7a083ce1df912aa107c006cbc18e5b8b25
Angad Sarpotdar <sixteefour@gmail.com>
2020-11-09 16:14:50 +0000

    Updates to python environment configuration settings.

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ questions = [
     Question(question_prompts[2], "b")
 ]
 
-def run_test
 
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer =input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(questions)
 
