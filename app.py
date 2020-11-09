103ef36503c0b7388bca313445649a800b30df7e
Angad Sarpotdar <sixteefour@gmail.com>
2020-11-09 15:49:49 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from Question import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+def run_test
+
+
